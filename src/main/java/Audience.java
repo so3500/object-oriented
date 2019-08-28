9b3b48430ed6967660d47caaf21afd8d797e26d1
so3500 <dltjddhs@gmail.com>
2019-08-28 23:27:04 +0900

    ch1 p21 TicketSeller와 Audience간 의존성 분리
    - Audience에서 ticket을 직접 삼

--- src/main/java/Audience.java
+++ src/main/java/Audience.java
@@ -1,9 +1,17 @@
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
 public class Audience {
-    @Getter
     private Bag bag;
 
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
--- src/main/java/TicketSeller.java
+++ src/main/java/TicketSeller.java
@@ -5,14 +5,6 @@ public class TicketSeller {
     private TicketOffice ticketOffice;
 
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
